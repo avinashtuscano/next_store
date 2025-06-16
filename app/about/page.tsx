46f4ce504236a254b911fccb338b83bdcc3f928c
avinash tuscano <avinashtuscano@gmail.comm>
2025-06-16 17:02:12 +0100

    about page check DB

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,20 @@
-function AboutPage() {
-  return <div>AboutPage</div>;
+import prisma from "@/utils/db";
+
+async function AboutPage() {
+  const task = await prisma.testProfile.create({
+    data: {
+      name: "random name",
+    },
+  });
+
+  const users = await prisma.testProfile.findMany();
+
+  return (
+    <div>
+      {users.map((user) => {
+        return <div key={user.id}>{user.name}</div>;
+      })}
+    </div>
+  );
 }
 export default AboutPage;
