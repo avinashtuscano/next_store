389cd2662577870537d6131e556e6e7a3a7cf201
avinash tuscano <avinashtuscano@gmail.comm>
2025-06-24 12:57:37 +0100

    added product model

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,19 +1,7 @@
-import prisma from "@/utils/db";
-
-async function AboutPage() {
-  const task = await prisma.testProfile.create({
-    data: {
-      name: "random name",
-    },
-  });
-
-  const users = await prisma.testProfile.findMany();
-
+function AboutPage() {
   return (
     <div>
-      {users.map((user) => {
-        return <div key={user.id}>{user.name}</div>;
-      })}
+      <h1>About Page</h1>
     </div>
   );
 }
