1ea10d2f91af9c59e3845047d95413d67e944fb2
Avinash Tuscano <avinash.tuscano@oup.com>
2025-06-27 17:35:43 +0100

    adding actions

--- components/global/EmptyList.tsx
+++ components/global/EmptyList.tsx
@@ -1,4 +1,16 @@
-function EmptyList() {
-  return <div>EmptyList</div>;
+import { cn } from "@/lib/utils";
+
+function EmptyList({
+  text = "No items to display",
+  className,
+}: {
+  text?: string;
+  className?: string;
+}) {
+  return (
+    <div>
+      <h2 className={cn("text-xl", className)}>{text}</h2>
+    </div>
+  );
 }
 export default EmptyList;
--- components/global/SectionTitle.tsx
+++ components/global/SectionTitle.tsx
@@ -1,4 +1,11 @@
-function SectionTitle() {
-  return <div>SectionTitle</div>;
+import { Separator } from "@/components/ui/separator";
+
+function SectionTitle({ text }: { text: string }) {
+  return (
+    <div>
+      <h2 className="text-3xl tracking-wider mb-8 font-medium">{text}</h2>
+      <Separator></Separator>
+    </div>
+  );
 }
 export default SectionTitle;
